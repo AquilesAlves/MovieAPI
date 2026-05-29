cf765aea9678282473c7b557125b4813175b9eeb
Aquiles Alves <aquilespereira789@gmail.com>
2026-05-29 20:07:27 -0300

    att

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
+const inputPesquisa = document.querySelector('#inputPesquisa')
+const btnPesquisa = document.querySelector('.btnPesquisa')
+let filmeLista = document.querySelector('.filmeLista')
 
 
 async function carregaFilme(pesquisa) {
-    const URL_API = `http://www.omdbapi.com/?s=${pesquisa}&page=1&apikey=`
+    const URL_API = `http://www.omdbapi.com/?s=${pesquisa}&page=1&apikey=38d482`
     const resposta = await fetch(`${URL_API}`)
     const dados = await resposta.json()
-    console.log(dados.Search)
+    //console.log(dados.Search)
+
+
+    
+    dados.Search.forEach(function(filme){
+        
+        const li = document.createElement('li')
+        li.innerHTML = `
+            <img src=" ${filme.Poster} " class='capaFilme'>
+            <p class="nomeFilme">  ${filme.Title}  </p>
+        `
+        filmeLista.appendChild(li)
+        
+
+    });
 }
 
-carregaFilme('harry potter')
+
+function enviaPesquisa(evento) {
+    if (evento.key && evento.key !== 'Enter') return
+
+    filmeLista.innerHTML = ''
+
+    const pesquisa = inputPesquisa.value
+    carregaFilme(pesquisa)
+    //console.log(pesquisa)        
+    
+}
+
+carregaFilme('star')
+carregaFilme('harry')
+
+
+inputPesquisa.addEventListener('keydown', enviaPesquisa)
+btnPesquisa.addEventListener('click', enviaPesquisa)
+
 
 // 38d482
 // https://www.youtube.com/watch?v=1VjdxCTBfUI
